ed83bbffd77f8a9aa34dee2cc38ded52554cdd22
Adel Baabbad <Baabbad.Adel@gmail.com>
2020-01-26 23:02:54 -0600

    Apply Factory Design Pattern

---
+++ ConversionSystemWithSOLIDandDesignPatterns/ConverterFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversionSystemWithSOLIDandDesignPatterns
+{
+    // Apply Factory Design Pattern
+    class ConverterFactory
+    {
+        public static Converter Create(BaseType baseType, int decimalNumber)
+        {
+            if (baseType == BaseType.Binary)
+            {
+                return new BinaryConverter(decimalNumber);
+            }
+            else if (baseType == BaseType.Octal)
+            {
+                return new OctalConverter(decimalNumber);
+            }
+            else if (baseType == BaseType.Hexadecimal)
+            {
+                return new HexadecimalConverter(decimalNumber);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
